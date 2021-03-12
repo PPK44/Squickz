c6440cc1cb75314140e765423ce21df7fed0bf3a
Squshy <36893334+Squshy@users.noreply.github.com>
2021-03-11 20:37:41 -0500

    monkey test dark theme xd

--- baller-squad/src/App.js
+++ baller-squad/src/App.js
@@ -1,14 +1,18 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-
+import { ThemeProvider } from "@material-ui/core/styles";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import theme from "./theme";
+import Item from "./components/item";
 
 function AppRouter() {
   return (
-    <Router>
-      <div>
-        hey
-      </div>
-    </Router>
+    <ThemeProvider theme={theme}>
+    <CssBaseline />
+      <Router>
+        <Item />
+      </Router>
+    </ThemeProvider>
   );
 }
 
