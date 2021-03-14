a06a2149b4ff2bdd82f8667f41584548f158a5b9
Squshy <36893334+Squshy@users.noreply.github.com>
2021-03-14 19:57:43 -0400

    yeah

--- baller-squad/src/app/pages/Home.js
+++ baller-squad/src/app/pages/Home.js
@@ -2,5 +2,5 @@
 import React from "react";
 
 export const Home = () => {
-  return <div className={'bg-simple-gray-1e'}>Hey</div>;
+  return <div className={"bg-simple-gray-1e flex flex-col w-full h-screen"}>hey</div>;
 };
