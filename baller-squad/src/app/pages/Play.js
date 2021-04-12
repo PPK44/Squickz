1ff8d45d1ead4bf1dbad9c51df8f734ef98c3aa7
Squshy <36893334+Squshy@users.noreply.github.com>
2021-04-12 14:26:04 -0400

    stop user from clicking over bounding box

--- baller-squad/src/app/pages/Play.js
+++ baller-squad/src/app/pages/Play.js
@@ -1,19 +1,30 @@
 import React, { useState, useEffect, useLayoutEffect, useRef } from "react";
 import { useTimer } from "../hooks/useTimer";
 
+const TIME_TO_PLAY = 5
+
 export const Play = () => {
   const boxRef = useRef();
   const containerRef = useRef();
+  const { seconds } = useTimer(0, TIME_TO_PLAY);
   const [clickHeight, setClickHeight] = useState(1);
   const [clicks, setClicks] = useState(0);
   const [currentHeight, setCurrentHeight] = useState(1);
+  const [gameDetails, setGameDetails] = useState({
+    clicks: 0,
+    hasWon: false,
+    gameLength: 0
+  });
 
   useLayoutEffect(() => {
     if (boxRef.current) {
       setCurrentHeight(boxRef.current.offsetHeight);
+      console.log("Height:", boxRef.current.offsetHeight);
     }
   }, []);
 
+  // Depending on screen height increment the box by a certain amount
+  // Kinda scales a lilbit with smaller screen and bigger screen sizes
   useEffect(() => {
     const { innerHeight: height } = window;
     if (height <= 650) {
@@ -28,15 +39,23 @@ export const Play = () => {
   }, []);
 
   const incrementHeight = () => {
+    const nextClickHeight = currentHeight + clickHeight;
+    const containerHeight = containerRef.current.offsetHeight;
     setClicks((c) => c + 1);
-    setCurrentHeight((h) => h + clickHeight);
+    if (containerHeight <= nextClickHeight) {
+      setCurrentHeight(containerHeight);
+      
+    } else {
+      setCurrentHeight((h) => h + clickHeight);
+    }
+    //if (seconds > 0) {
+
+    //}
   };
 
   const style = {
     height: currentHeight,
   };
-
-  const [minutes, seconds] = useTimer(0, 5);
 
   return (
     <div className={`w-full h-full p-5`}>
@@ -47,8 +66,13 @@ export const Play = () => {
         <div className={`flex flex-col items-left justify-center h-full`}>
           <p className={`text-3xl`}>Clicks: {clicks}</p>
           <p className={`text-3m`}>Current Inc: {clickHeight}</p>
-          <p className={`text-3m`}>Minutes: {minutes}</p>
-          <p className={`text-3m`}>Seconds: {seconds}</p>
+          <p
+            className={`text-3m ${
+              seconds > 0 ? "text-green-400" : "text-red-600"
+            }`}
+          >
+            Seconds: {seconds}
+          </p>
         </div>
         <div className={`flex flex-col-reverse flex1`}>
           <div
