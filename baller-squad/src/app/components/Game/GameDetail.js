7226995372f9dc7165480c36dcaeec0bad793f58
Squshy <36893334+Squshy@users.noreply.github.com>
2021-04-17 16:59:02 -0400

    changed color values

--- baller-squad/src/app/components/Game/GameDetail.js
+++ baller-squad/src/app/components/Game/GameDetail.js
@@ -3,13 +3,13 @@ import React from "react";
 export const GameDetail = ({ text, value, color }) => {
   return (
     <div
-      className={`flex-col flex items-center justify-center h-auto w-auto p-5 rounded-xl bg-simple-gray-29 border-4 border-${color} w-full divide-solid divide-y divide-${color}`}
+      className={`flex-col flex items-center justify-center h-auto w-auto p-5 rounded-xl bg-simple-gray-29 border-4 border-${color}-700 w-full divide-solid divide-y divide-${color}-400`}
     >
       <div className={`w-full items-center justify-center flex p-2`}>
-        <p className={`lg:text-4xl text-xl text-${color}`}>{text}</p>
+        <p className={`lg:text-4xl text-xl text-${color}-400`}>{text}</p>
       </div>
       <div className={`w-full items-center justify-center flex p-2`}>
-        <p className={`lg:text-3xl text-lg text-${color}`}>{value}</p>
+        <p className={`lg:text-3xl text-lg text-${color}-400`}>{value}</p>
       </div>
     </div>
   );
--- baller-squad/src/app/components/Game/GameDetails.js
+++ baller-squad/src/app/components/Game/GameDetails.js
@@ -4,9 +4,9 @@ import { GameDetail } from './GameDetail';
 export const GameDetails = ({clicks, maxClicks, increment}) => {
   return (
     <div className={`w-full h-full flex flex-col-reverse gap-4`}>
-      <GameDetail text={`Increment`} value={increment} color={`pink-400`} />
-      <GameDetail text={`Max Clicks`} value={maxClicks} color={`blue-400`} />
-      <GameDetail text={`Clicks`} value={clicks} color={`purple-400`} />
+      <GameDetail text={`Increment`} value={increment} color={`pink`} />
+      <GameDetail text={`Max Clicks`} value={maxClicks} color={`blue`} />
+      <GameDetail text={`Clicks`} value={clicks} color={`purple`} />
     </div>
   )
 }
