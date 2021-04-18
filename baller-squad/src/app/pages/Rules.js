88eb1098430d26a86160007f64589b73589bcce0
Henry Zheng <HenryZheng1998@gmail.com>
2021-04-18 17:20:14 -0400

    Rules now looks great on mobile

--- baller-squad/src/app/pages/Rules.js
+++ baller-squad/src/app/pages/Rules.js
@@ -9,7 +9,7 @@ export const Rules = () => {
     <div className="w-full h-full p-5">
       <div className="bg-simple-gray-29 rounded-lg divide-y-4 divide-blue-500">
       <div className="text-5xl text-center p-4">How to play:</div>
-        <div class="flex flex-row items-center p-5 justify-around">
+        <div class="flex lg:flex-row flex-col items-center p-5 justify-around">
             <div className="box-border h-40 w-40 p-4 border-pink-500 border-4 m4 rounded-lg"> 
                 Click Start game on the bottom left to start the game
             </div>
