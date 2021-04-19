4ecbe0e30da0ad589346e66faeda2eddb9600c91
Squshy <36893334+Squshy@users.noreply.github.com>
2021-04-19 14:13:35 -0400

    fixed

--- baller-squad/src/app/pages/Rules.js
+++ baller-squad/src/app/pages/Rules.js
@@ -9,7 +9,7 @@ export const Rules = () => {
       <div className="w-full h-full p-5">
         <div className="bg-simple-gray-29 rounded-lg divide-y-4 divide-blue-500">
         <div className="text-5xl text-center p-4">How to play:</div>
-          <div class="flex lg:flex-row flex-col items-center p-5 justify-around h-auto">
+          <div className="flex lg:flex-row flex-col items-center p-5 justify-around h-auto">
               <div className="box-border h-full w-40 p-4 border-pink-500 border-4 m4 rounded-lg"> 
                   Click Start game on the bottom left to start the game
               </div>
