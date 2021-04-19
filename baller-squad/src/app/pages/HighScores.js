9f0f3f55fb9e95b5b791f5fc17063d588eec6299
PPK44 <pauljr4411@gmail.com>
2021-04-18 23:59:11 -0400

    highscore data displaying in graph

--- baller-squad/src/app/pages/HighScores.js
+++ baller-squad/src/app/pages/HighScores.js
@@ -1,5 +1,5 @@
 // Home page for app
-import React, {useEffect, useState, useContext} from "react";
+import React, {useEffect, useRef,useState, useContext} from "react";
 import * as d3 from "d3";
 import { UserContext } from "../userContext";
 const data = [{"grade":'A', "frequency": 0.5},
@@ -11,27 +11,40 @@ const data = [{"grade":'A', "frequency": 0.5},
 export const HighScores = () => {
 
   const {userInfo, setUserInfo} = useContext(UserContext);
-  
+  const time10 = [];
+  const time15 = [];
+  const time20 = [];
   useEffect(() => {
     getHighScoreData();
-    drawChart(data, "graph");
-    drawChart(data, "graph1");
-    drawChart(data, "graph2");
-  }, [data])
+    // drawChart(data, "graph");
+    // drawChart(data, "graph1");
+    // drawChart(data, "graph2");
+  }, [time10])
 
   const getHighScoreData = () => {
-    console.log(typeof userInfo.userName);
     fetch(`http://localhost:3000/getHighScores?username=${userInfo.userName}`)
     .then((res) => res.json())
     .then(res =>{
-        console.log(res);
+      res.forEach(element => {
+        if(element.time === "10"){
+          time10.push(element);
+        }else if(element.time === "15"){
+          time15.push(element);
+        }else if(element.time === "20"){
+          time20.push(element);
+        }
+      });
+      drawChart(time10, "graph", time10[0].time);
+      drawChart(time15, "graph1", time15[0].time);
+      drawChart(time20, "graph2", time20[0].time);
+        
    
     });
   }
 
   
 
-  const drawChart = (data, graph) =>{
+  const drawChart = (data, graph, time) =>{
     const margin = 70;
     const width = 500;
     const height = 400;
@@ -42,20 +55,20 @@ export const HighScores = () => {
         .attr("width", width)
         .attr("height", height);
     svg.append("text")
-        .attr("transform", "translate(100,0)")
+        .attr("transform", "translate(30,0)")
         .attr("x", 50)
         .attr("y", 50)
         .attr("font-size", "24px")
         .style('fill', 'white')
-        .text("High Scores");
+        .text("High Score for " + time + " seconds");
     const chart = svg.append('g')
         .attr('transform', `translate(${margin}, ${margin})`);
 
     var xScale = d3.scaleBand().range([0, chartWidth]).padding(0.4),
     yScale = d3.scaleLinear().range([chartHeight, 0]);
 
-    xScale.domain(data.map(function(d){return d.grade;}));
-    yScale.domain([0, d3.max(data, function(d) { return d.frequency; })+0.1]);
+    xScale.domain(data.map(function(d){return d.level;}));
+    yScale.domain([0, d3.max(data, function(d) { return d.score; })+0.1]);
     chart.append('g')
         .attr('transform', `translate(0, ${chartHeight})`)
         .call(d3.axisBottom(xScale));
@@ -86,11 +99,11 @@ export const HighScores = () => {
          .data(data)
          .enter().append("rect")
          .attr("class", "bar")
-         .attr("x", function(d) { return xScale(d.grade); })
-         .attr("y", function(d) { return yScale(d.frequency); })
+         .attr("x", function(d) { return xScale(d.level); })
+         .attr("y", function(d) { return yScale(d.score); })
          .attr("width", xScale.bandwidth())
-         .attr("height", function(d) { return chartHeight - yScale(d.frequency); })
-         .attr("fill", (data) => colourScale(data.frequency));
+         .attr("height", function(d) { return chartHeight - yScale(d.score); })
+         .attr("fill", (data) => colourScale(data.score));
     } 
 
   return (
