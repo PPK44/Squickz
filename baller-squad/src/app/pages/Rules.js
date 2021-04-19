9f6c9883f9d0ba8a47ae64fcfbfea48cae225723
Squshy <36893334+Squshy@users.noreply.github.com>
2021-04-19 14:05:03 -0400

    swagu

--- baller-squad/src/app/pages/Rules.js
+++ baller-squad/src/app/pages/Rules.js
@@ -1,6 +1,5 @@
-// Home page for app
+// Rule page
 import React from "react";
-import { TopNavModule } from "./TopNav";
 import { useFetch } from "../components/KanyeAPI/KanyeFetch";
 
 export const Rules = () => {
@@ -27,20 +26,5 @@ export const Rules = () => {
 
       </div>
     </div>
-    
-    // <div className="w-full h-full p-5">
-    //   <div className="border-4 border-yellow-300">
-    //     <div className="flex flex-col items-center justify-center p-5">
-    //         <div className="text-5xl">Rules:</div>
-    //         <div className="text-2xl p-5">Click Start game on the bottom left to start the game</div>
-    //         <div className="text-2xl p-5">Click as many times as you can before the time runs out</div>
-    //         <div className="text-2xl p-5">Sign up to save your high score and compare yourself to others!</div>
-    //         <div className="text-4xl p-5">Have fun!</div>
-    //         <div className="flex-col-reversed pt-40">
-    //             <div className="text-2xl">Kanye: "{loading ? "loading..." : data}"</div>
-    //         </div>
-    //     </div>
-    //   </div>
-    // </div>
     )
 };
