6d1cf1df0777f7e61b99660eb589fb8ffbb004d9
Henry Zheng <HenryZheng1998@gmail.com>
2021-04-19 00:02:58 -0400

    Changed poop to copyright

--- baller-squad/src/app/components/Nav/Footer.js
+++ baller-squad/src/app/components/Nav/Footer.js
@@ -9,10 +9,10 @@ export const Footer = () => {
   const { data, loading } = useFetch("https://api.kanye.rest/?format=text");
 
   return (
-    <footer className="p-5 w-full bottom-0 inset-x-0 sm:px-6 lg:px-8 bg-simple-gray-1e">
+    <footer className="p-5 w-full absolute bottom-0 inset-x-0 sm:px-6 lg:px-8 bg-simple-gray-1e">
         <div className="text-xl text-center">Kanye: "{loading ? "loading..." : data}"</div>
         <div className="flex flex-row flex1 justify-between">
-            <div className="flex">Poop</div>
+            <div className="flex">Copyright 2021</div>
             <div className="flex flex-row-reverse gap-4">
               <a href="#">
                 <img src="https://img.icons8.com/fluent/32/000000/twitter.png"/>    
