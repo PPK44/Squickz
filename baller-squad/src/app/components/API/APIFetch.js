d4b74087837c65e089cacc529716314e0c4e6556
Henry Zheng <HenryZheng1998@gmail.com>
2021-04-20 12:50:16 -0400

    Commented on APIFetch function

--- baller-squad/src/app/components/API/APIFetch.js
+++ baller-squad/src/app/components/API/APIFetch.js
@@ -1,5 +1,9 @@
 import { useEffect, useState } from "react";
 
+// Custom hook to get the api and its status
+// User inserts a url
+// Once the data has loaded it then sets the data to y(which is our text from the api) and loading to false
+// This is used in Footer.js
 export const useFetch = url => {
     const [state, setState] = useState({ data:null, loading: true});
     
